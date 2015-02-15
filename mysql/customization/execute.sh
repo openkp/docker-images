07620ed445b1bb0039cdc26b3f68a9d1ac67f3b2
andrzejszywala <andrzej.szywala@gmail.com>
2015-02-15 06:16:49 -0800

    konfiguracja obrazow dockera dla openkp

--- mysql/customization/execute.sh
+++ mysql/customization/execute.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
-
-# Usage: execute.sh [WildFly mode] [configuration file]
-#
-# The default mode is 'standalone' and default configuration is based on the
-# mode. It can be 'standalone.xml' or 'domain.xml'.
-
-JBOSS_HOME=/opt/jboss/wildfly
+JBOSS_HOME=/opt/wildfly/openkp
 JBOSS_CLI=$JBOSS_HOME/bin/jboss-cli.sh
 JBOSS_MODE=${1:-"standalone"}
 JBOSS_CONFIG=${2:-"$JBOSS_MODE.xml"}
@@ -17,7 +11,7 @@ function wait_for_server() {
 }
 
 echo "=> Starting WildFly server"
-$JBOSS_HOME/bin/$JBOSS_MODE.sh -b 0.0.0.0 -c $JBOSS_CONFIG &
+$JBOSS_HOME/bin/$JBOSS_MODE.sh -b 0.0.0.0 -c $JBOSS_CONFIG & 
 
 echo "=> Waiting for the server to boot"
 wait_for_server
@@ -29,27 +23,19 @@ echo "=> MYSQL (docker host): " $DB_PORT_3306_TCP_ADDR
 echo "=> MYSQL (docker port): " $DB_PORT_3306_TCP_PORT
 echo "=> MYSQL (kubernetes host): " $MYSQL_SERVICE_HOST
 echo "=> MYSQL (kubernetes port): " $MYSQL_SERVICE_PORT
-#$JBOSS_CLI -c --file=`dirname "$0"`/commands.cli
 $JBOSS_CLI -c << EOF
 batch
-
-# Add MySQL module
-module add --name=com.mysql --resources=/opt/jboss/wildfly/customization/mysql-connector-java-5.1.34.jar --dependencies=javax.api,javax.transaction.api
-
-# Add MySQL driver
+# dodanie modulu dla sterownika mysql
+module add --name=com.mysql --resources=/opt/wildfly/customization/mysql-connector-java-5.1.34.jar --dependencies=javax.api,javax.transaction.api
+# dodanie sterownika mysql
 /subsystem=datasources/jdbc-driver=mysql:add(driver-name=mysql,driver-module-name=com.mysql,driver-xa-datasource-class-name=com.mysql.jdbc.jdbc2.optional.MysqlXADataSource)
-
-# Add the datasource
-#data-source add --name=mysqlDS --driver-name=mysql --jndi-name=java:jboss/datasources/OpenKPMySQLDS --connection-url=jdbc:mysql://$MYSQL_HOST:$MYSQL_PORT/sample?useUnicode=true&amp;characterEncoding=UTF-8 --user-name=mysql --password=mysql --use-ccm=false --max-pool-size=25 --blocking-timeout-wait-millis=5000 --enabled=true
-data-source add --name=mysqlDS --driver-name=mysql --jndi-name=java:jboss/datasources/OpenKPMySQLDS --connection-url=jdbc:mysql://$MYSQL_SERVICE_HOST:$MYSQL_SERVICE_PORT/sample?useUnicode=true&amp;characterEncoding=UTF-8 --user-name=mysql --password=mysql --use-ccm=false --max-pool-size=25 --blocking-timeout-wait-millis=5000 --enabled=true
-#data-source add --name=mysqlDS --driver-name=mysql --jndi-name=java:jboss/datasources/OpenKPMySQLDS --connection-url=jdbc:mysql://$DB_PORT_3306_TCP_ADDR:$DB_PORT_3306_TCP_PORT/sample?useUnicode=true&amp;characterEncoding=UTF-8 --user-name=mysql --password=mysql --use-ccm=false --max-pool-size=25 --blocking-timeout-wait-millis=5000 --enabled=true
+# dodanie datasourca
+data-source add --name=mysqlDS --driver-name=mysql --jndi-name=java:jboss/datasources/OpenKPMySQLDS --connection-url=jdbc:mysql://$DB_PORT_3306_TCP_ADDR:$DB_PORT_3306_TCP_PORT/sample?useUnicode=true&amp;characterEncoding=UTF-8 --user-name=mysql --password=mysql --use-ccm=false --max-pool-size=25 --blocking-timeout-wait-millis=5000 --enabled=true
+# ustawienie datasourca mysql jako domyslnego
 /subsystem=ee/service=default-bindings:write-attribute(name="datasource", value="java:jboss/datasources/OpenKPMySQLDS")
 # Execute the batch
 run-batch
 EOF
-
-# Deploy the WAR
-cp /opt/jboss/wildfly/customization/openkp.war $JBOSS_HOME/$JBOSS_MODE/deployments/openkp.war
 
 echo "=> Shutting down WildFly"
 if [ "$JBOSS_MODE" = "standalone" ]; then
@@ -57,6 +43,8 @@ if [ "$JBOSS_MODE" = "standalone" ]; then
 else
   $JBOSS_CLI -c "/host=*:shutdown"
 fi
+#deploy
+cp /opt/wildfly/customization/openkp.war $JBOSS_HOME/$JBOSS_MODE/deployments/openkp.war
 
 echo "=> Restarting WildFly"
 $JBOSS_HOME/bin/$JBOSS_MODE.sh -b 0.0.0.0 -c $JBOSS_CONFIG
